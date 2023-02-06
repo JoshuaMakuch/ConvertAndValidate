21eeb0011490a71e0e0b139c18efcbd89dcca2fd
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-02-06 08:11:05 -0700

    Add project files.

---
+++ ConvertAndValidate/ConvertAndValidate.vb
@@ -0,0 +1,19 @@
+'Joshua Makuch
+'RCET 0265
+'Fall 2023
+'Convert And Validate
+'
+
+Option Strict On
+Option Explicit On
+
+Imports System
+
+Module ConvertAndValidate
+    Sub Main(args As String())
+
+
+
+    End Sub
+
+End Module
